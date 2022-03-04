3052b4a25218513fd14ae67da965d0fbf8495ae4
jawad-nsu <jawadurrahim1997@gmail.com>
2022-03-04 16:34:43 +0600

    successfully fetching user data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
-
+const url = require("url");
 require("dotenv").config();
 //working port
 
@@ -18,7 +18,8 @@ async function idGenerator() {
   const responseUrl = await axios.post(
     url,
     {
-      visible_text: "hello",
+      visible_text:
+        "Welcome to EUCaps. Please provide your password to continue verification process",
     },
     {
       auth: {
@@ -39,6 +40,41 @@ async function hmacGenerator() {
 
   return hash;
 }
+
+app.get("/idHash", async (req, res) => {
+  const id = await idGenerator();
+  const getHash = await hmacGenerator();
+  const authUrl =
+    "https://demo-api.gii.cloud/api/oauth/auth" +
+    "?client_id=eucaps_test" + // your-client is the client ID provided by DID
+    "&redirect_uri=" +
+    encodeURIComponent("http://localhost:5000/auth") +
+    "&response_type=code" +
+    "&scope=openid" +
+    `&sign_id=${id}` + // ID retrieved from post above
+    `&state=${getHash}` + // generate state on your side, using e.g. a HMAC
+    "&identity_provider=bankid-se" +
+    "&display=popup";
+
+  // res.redirect(authUrl);
+  const htmlIframe = `<iframe style="height: 500px; width: 500px" src=${authUrl}></iframe>`;
+  const htmlBroilerPlate = `
+                            <!DOCTYPE html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="UTF-8" />
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                                <title>Document</title>
+                              </head>
+                              <body style="background-color:black; color:white;">
+                                <h3>DevCode Identity</h3>
+                                ${htmlIframe}
+                              </body>
+                            </html>
+`;
+  res.send(htmlBroilerPlate);
+});
 
 app.get("/", async (req, res) => {
   const id = await idGenerator();
@@ -76,6 +112,46 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/auth", async (req, res) => {
-  console.log(req.query);
-  res.send("successfully done authentication");
+  const { code } = req.query;
+  const access_token = await generateAccessToken(code);
+  const user_info = await getUserProfile(access_token);
+  console.log("user_info", user_info);
+  res.send(user_info);
 });
+
+const generateAccessToken = async (code) => {
+  const baseUrl = "https://demo-api.gii.cloud/api/oauth/token";
+  const params = new url.URLSearchParams({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: "http://localhost:5000/auth",
+  });
+  const data = params.toString();
+  const responseUrl = await axios({
+    method: "post",
+    url: baseUrl,
+    data,
+    auth: {
+      username: process.env.DEVCODE_USERNAME,
+      password: process.env.DEVCODE_SECRET,
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+  });
+  return responseUrl.data.access_token;
+};
+
+const getUserProfile = async (access_token) => {
+  const res = await axios({
+    method: "post",
+    url: "https://demo-api.gii.cloud/api/oauth/userinfo",
+    data: {
+      scopes: "profile",
+    },
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  return res.data;
+};
